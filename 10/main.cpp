c8658cbdf489effb913311cf0feab657d6761522
Oscilous <artis.fils@gmail.com>
2023-04-14 15:46:51 +0200

    Finished a b from exercise

--- 10/main.cpp
+++ 10/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <functional>
-
+    
 using namespace std;
 
 //First part
@@ -9,7 +9,7 @@ void increase(int& sum, int delta){ sum += delta;}
 
 //Second part
 class Factor_functor{
-    
+
     int factor;
 public:
     Factor_functor(int f) : factor(f) {}
@@ -22,17 +22,20 @@ int main(void){
     auto f_ptr = increase;
     int a = 4;
     f_ptr(a,6);
-    cout << "Result: " << a << endl;
+    cout << "Result: " << a << endl << endl;
 
     //Second part
     Factor_functor twice(2), tripple(3), quadruple(4);  
     int b = 3;
-    cout << "Twice of a is " << twice(b) << endl;
-    cout << "Tripple of a is " << tripple(b) << endl;
-    cout << "Quadtruple of a is " << quadruple(b) << endl;
-    cout << "Tenfold of a is " << Factor_functor(10)(a) << endl;
-
-    //Lambda expressions
-    
+    int twenty = 20;
+    cout << "Twice of b is " << twice(b) << endl;
+    cout << "Tripple of b is " << tripple(b) << endl;
+    cout << "Quadtruple of b is " << quadruple(b) << endl;
+    cout << "Tenfold of b is " << Factor_functor(10)(a) << endl << endl;
+    //Lambda expressions 
+    int fifty = 50;
+    auto fiftyfold =  [fifty] (int x) { return x * fifty;} ; //Named lamda
+    cout << "Twentyfold of b is " << [twenty] (int x) { return x * twenty;}(b) << endl;
+    cout << "Fiftyfold of b is " << fiftyfold(b) << endl;
     return 0;
 }
